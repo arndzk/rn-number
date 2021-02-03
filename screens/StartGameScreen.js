c4a37ca0208d87b241d073375cb4bd954edc80ef
arndzk <22920826+arndzk@users.noreply.github.com>
2021-02-03 17:26:12 +0200

    work on StartGameScreen layout

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
 
 const StartGameScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>The Game Begins!</Text>
+      <Text style={styles.title}>Start a New Game</Text>
+      <View style={styles.inputContainer}>
+        <Text>Select a Number</Text>
+        <TextInput style={styles.numberInput} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <View style={styles.button}>
+          <Button color="#ff6961" title="Reset" onPress={() => {}} />
+        </View>
+        <View style={styles.button}>
+          <Button color="#ff6961" title="Confirm" onPress={() => {}} />
+        </View>
+      </View>
     </View>
   );
 };
@@ -14,6 +26,30 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: '80%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  numberInput: {
+    marginHorizontal: 5,
+    width: '60%',
+  },
+  buttonContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  button: {
+    width: '40%',
   },
 });
 
